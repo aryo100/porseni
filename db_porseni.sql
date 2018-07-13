8fc55497c0aca77c3a7534573a03a1f4d5ce9901
aryo100 <34300267+aryo100@users.noreply.github.com>
2018-07-13 09:19:39 +0700

    database

--- db_porseni.sql
+++ db_porseni.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 29 Jun 2018 pada 15.51
+-- Generation Time: 13 Jul 2018 pada 04.17
 -- Versi Server: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -42,11 +42,8 @@ CREATE TABLE `akun` (
 --
 
 INSERT INTO `akun` (`id_akun`, `id_user`, `username`, `password`, `status`, `pt`) VALUES
-(9, 0, 're_01', 'fa2bd3306b93e30c96b21b4cc75fa4d7', 'institusi', 're'),
-(21, 0, 'POLBAN_01', '6a0c9dcbffbbe7dfcabfa278ec33064d', 'institusi', 'Politeknik Bandung'),
-(22, 0, 'polnes', 'c8f2b53efd6d10ca0892e104b684c1df', 'institusi', 'Politeknik Nusantara'),
-(23, 0, 'polma', 'af619621d0f584d194722f14508c0677', 'institusi', 'Politeknik Malang'),
-(24, 0, 'admin', '5701e5352e3c42a691df7cb3466bc044', 'admin', 'master');
+(24, 0, 'admin', '5701e5352e3c42a691df7cb3466bc044', 'admin', 'master'),
+(28, 1, 'aryo100@gmail.com', '5701e5352e3c42a691df7cb3466bc044', 'institusi', 'dsa');
 
 -- --------------------------------------------------------
 
@@ -61,6 +58,8 @@ CREATE TABLE `atlet` (
   `gender` varchar(1) NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `umur` int(3) NOT NULL,
+  `berat` int(11) NOT NULL,
+  `tinggi` int(11) NOT NULL,
   `no_hp` varchar(15) NOT NULL,
   `email` varchar(100) NOT NULL,
   `pt` varchar(100) NOT NULL,
@@ -74,24 +73,25 @@ CREATE TABLE `atlet` (
 -- Dumping data untuk tabel `atlet`
 --
 
-INSERT INTO `atlet` (`id_atlet`, `nama`, `npm`, `gender`, `tanggal_lahir`, `umur`, `no_hp`, `email`, `pt`, `foto`, `ss`, `cabang`, `status`) VALUES
-(4, 'Yohanes A M', '4616010031', 'L', '1999-08-23', 0, '02100727', 'yohanes@gmail.com', 'PNJ', 'uti.png', 'pp.png', 'Tennis', 'approved'),
-(5, 'yo', '4516010031', 'L', '1999-07-31', 0, '081xxxxxxxxx', 'y0@mail.com', 'PNJ', 'tot', 'tot', 'Basket', 'approved'),
-(6, 'apa', '1234567890', 'P', '2018-04-26', 0, '0211992', 'apa@gmail.com', 'IPB', '176779-200.png', 'pp.png', 'Basket', 'approved'),
-(7, 'u', '4616010030', 'P', '2018-04-26', 0, '0321321', 'u@mail.com', 'UNI', '176779-200.png', 'ads.pnj', 'Bulu tangkis', 'approved'),
-(8, 'adsd', '3213213', 'P', '2018-03-27', 0, '3221312', 'dassa', 'OI', '366778-200.png', 'sdsa.kl', 'Takrau', 'unapproved'),
-(10, 'dasd', '31321', 'P', '2018-04-23', 0, '13213123', 'dfsd@fdsa.dasd', 're', '176779-200.png', 'das.asd', 'Tennis', 'approved'),
-(11, 'km', '2213', 'L', '2018-04-24', 0, '23213123', 'aad@dsd.das', 're', '366778-2001.png', 'das.asd', 'Basket', 'approved'),
-(12, 'mmm', '5465454', 'L', '2018-05-21', 0, '02100727', 'aad@dsd.das', 're', '21.png', 'bhjb', 'Bulu tangkis', 'unapproved'),
-(13, 'Mujahd', '7987987', 'L', '2018-05-28', 0, '02100727', 'aryo100@gmail.com', 're', '25.png', 'a', 'Bulu tangkis', 'unapproved'),
-(14, 'Aryo', '02323123', 'L', '2000-09-28', 0, '02100727', 'aryo100@gmail.com', 're', '23.png', '339A2F4F00000578-0-image-a-7_14617979386461.jpg', 'Bulu tangkis', 'unapproved'),
-(16, 'Yohanes Bet', '321321', 'P', '2018-05-01', 0, '02100727', 'aryo100@gmail.com', 're', '176779-20015.png', '339A2F4F00000578-0-image-a-7_146179793864610.jpg', 'Bulu tangkis', 'unapproved'),
-(17, 'Ted', '4616010031', 'L', '1999-08-28', 0, '0211992', 'aad@dsd.das', 're', 'canva-kawaii-koala-animal-icon-MACTmnhQTYQ2.png', '2.png', 'Bulu tangkis', 'unapproved'),
-(18, 'we', '2213', 'L', '2000-08-28', 0, '0211992', 'aad@dsd.das', 're', '', '339A2F4F00000578-0-image-a-7_146179793864612.jpg', 'Bulu tangkis', 'unapproved'),
-(20, 'Aryo Mujahid', '321321323', 'L', '2000-09-28', 0, '02100727', 'aryo100@gmail.com', 're', '176779-20016.png', '339A2F4F00000578-0-image-a-7_146179793864614.jpg', 'Bulu tangkis', 'unapproved'),
-(21, 'jhj', '45678', 'P', '1999-08-28', 0, '345678', 'ar@c', 'Institut Teknologi Bandung', '581.jpg', '339A2F4F00000578-0-image-a-7_146179793864615.jpg', 'Basket', 'unapproved'),
-(22, 'PIP', '67890', 'L', '1999-09-30', 0, '02100727', 'aryo100@gmail.com', 're', '176779-20017.png', 'maxresdefault.jpg', 'Basket', 'unapproved'),
-(23, 'Map', '34567890', 'L', '1999-08-04', 0, '0987654', 'aryo100@gmail.com', 'Institut Teknologi Bandung', '176779-20018.png', '339A2F4F00000578-0-image-a-7_146179793864616.jpg', 'Bulu tangkis', 'approved');
+INSERT INTO `atlet` (`id_atlet`, `nama`, `npm`, `gender`, `tanggal_lahir`, `umur`, `berat`, `tinggi`, `no_hp`, `email`, `pt`, `foto`, `ss`, `cabang`, `status`) VALUES
+(4, 'Yohanes A M', '4616010031', 'L', '1999-08-23', 0, 0, 0, '02100727', 'yohanes@gmail.com', 'PNJ', 'uti.png', 'pp.png', 'Tennis', 'approved'),
+(5, 'yo', '4516010031', 'L', '1999-07-31', 0, 0, 0, '081xxxxxxxxx', 'y0@mail.com', 'PNJ', 'tot', 'tot', 'Basket', 'approved'),
+(6, 'apa', '1234567890', 'P', '2018-04-26', 0, 0, 0, '0211992', 'apa@gmail.com', 'IPB', '176779-200.png', 'pp.png', 'Basket', 'approved'),
+(7, 'u', '4616010030', 'P', '2018-04-26', 0, 0, 0, '0321321', 'u@mail.com', 'UNI', '176779-200.png', 'ads.pnj', 'Bulu tangkis', 'approved'),
+(8, 'adsd', '3213213', 'P', '2018-03-27', 0, 0, 0, '3221312', 'dassa', 'OI', '366778-200.png', 'sdsa.kl', 'Takrau', 'unapproved'),
+(10, 'dasd', '31321', 'P', '2018-04-23', 0, 0, 0, '13213123', 'dfsd@fdsa.dasd', 're', '176779-200.png', 'das.asd', 'Tennis', 'approved'),
+(11, 'km', '2213', 'L', '2018-04-24', 0, 0, 0, '23213123', 'aad@dsd.das', 're', '366778-2001.png', 'das.asd', 'Basket', 'approved'),
+(12, 'mmm', '5465454', 'L', '2018-05-21', 0, 0, 0, '02100727', 'aad@dsd.das', 're', '21.png', 'bhjb', 'Bulu tangkis', 'unapproved'),
+(13, 'Mujahd', '7987987', 'L', '2018-05-28', 0, 0, 0, '02100727', 'aryo100@gmail.com', 're', '25.png', 'a', 'Bulu tangkis', 'unapproved'),
+(14, 'Aryo', '02323123', 'L', '2000-09-28', 0, 0, 0, '02100727', 'aryo100@gmail.com', 're', '23.png', '339A2F4F00000578-0-image-a-7_14617979386461.jpg', 'Bulu tangkis', 'unapproved'),
+(16, 'Yohanes Bet', '321321', 'P', '2018-05-01', 0, 0, 0, '02100727', 'aryo100@gmail.com', 're', '176779-20015.png', '339A2F4F00000578-0-image-a-7_146179793864610.jpg', 'Bulu tangkis', 'unapproved'),
+(17, 'Ted', '4616010031', 'L', '1999-08-28', 0, 0, 0, '0211992', 'aad@dsd.das', 're', 'canva-kawaii-koala-animal-icon-MACTmnhQTYQ2.png', '2.png', 'Bulu tangkis', 'unapproved'),
+(18, 'we', '2213', 'L', '2000-08-28', 0, 0, 0, '0211992', 'aad@dsd.das', 're', '', '339A2F4F00000578-0-image-a-7_146179793864612.jpg', 'Bulu tangkis', 'unapproved'),
+(20, 'Aryo Mujahid', '321321323', 'L', '2000-09-28', 0, 0, 0, '02100727', 'aryo100@gmail.com', 're', '176779-20016.png', '339A2F4F00000578-0-image-a-7_146179793864614.jpg', 'Bulu tangkis', 'unapproved'),
+(21, 'jhj', '45678', 'P', '1999-08-28', 0, 0, 0, '345678', 'ar@c', 'Institut Teknologi Bandung', '581.jpg', '339A2F4F00000578-0-image-a-7_146179793864615.jpg', 'Basket', 'unapproved'),
+(22, 'PIP', '67890', 'L', '1999-09-30', 0, 0, 0, '02100727', 'aryo100@gmail.com', 're', '176779-20017.png', 'maxresdefault.jpg', 'Basket', 'unapproved'),
+(23, 'Map', '34567890', 'L', '1999-08-04', 0, 0, 0, '0987654', 'aryo100@gmail.com', 'Institut Teknologi Bandung', '176779-20018.png', '339A2F4F00000578-0-image-a-7_146179793864616.jpg', 'Bulu tangkis', 'approved'),
+(24, 'Aryo', '4616010023', 'L', '2000-09-28', 0, 80, 170, '082299480039', 'aryo100@gmail.com', 'dsa', '582.jpg', '339A2F4F00000578-0-image-a-7_146179793864619.jpg', 'Tennis', '');
 
 -- --------------------------------------------------------
 
@@ -148,8 +148,17 @@ CREATE TABLE `institusi` (
   `nama_pj` varchar(255) NOT NULL,
   `no_telp_pj` int(20) NOT NULL,
   `ss_bukti_pay` varchar(500) NOT NULL,
+  `ss_bukti_atlet` varchar(255) NOT NULL,
+  `n_atlet` int(11) NOT NULL,
   `status` enum('pendding','approved','unapproved','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `institusi`
+--
+
+INSERT INTO `institusi` (`id_pt`, `email`, `nama_pt`, `alamat_pt`, `no_telp_pt`, `nama_pj`, `no_telp_pj`, `ss_bukti_pay`, `ss_bukti_atlet`, `n_atlet`, `status`) VALUES
+(1, 'aryo100@gmail.com', 'dsa', 'jl.c', 2147483647, 'jono widyanata', 2147483647, '27.png', '339A2F4F00000578-0-image-a-7_14617979386461.jpg', 200, 'approved');
 
 --
 -- Indexes for dumped tables
@@ -193,13 +202,13 @@ ALTER TABLE `institusi`
 -- AUTO_INCREMENT for table `akun`
 --
 ALTER TABLE `akun`
-  MODIFY `id_akun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id_akun` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `atlet`
 --
 ALTER TABLE `atlet`
-  MODIFY `id_atlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id_atlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `berita`
@@ -217,7 +226,7 @@ ALTER TABLE `gallery`
 -- AUTO_INCREMENT for table `institusi`
 --
 ALTER TABLE `institusi`
-  MODIFY `id_pt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_pt` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
